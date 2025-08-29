b95cc1fd0a2592fea5ff39abf82ec09b63a14c0d
Ridmi Samarasundara <ridmisamarasundara@gmail.com>
2025-08-29 18:11:48 +0530

    Add files via upload

---
+++ Day01_08.21/prg1.pl
@@ -0,0 +1,15 @@
+food(pizza).
+bird(parrot).
+island(srilanka).
+
+animal(dog).
+fruit(kiwi).
+weapon(gun).
+hardware(keyboard).
+android(samsung).
+male(kamal).
+student(john).
+black(elephant).
+shape(circle).
+vehicle(bus).
+
